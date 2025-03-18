67ef7a23a0bedfad67b0fd0eb66c8b18e23a646e
nguyenkimky <kynkph46127@fpt.edu.vn>
2025-03-18 11:22:09 +0700

    productDetail

--- DUAN/src/pages/user/theme/header/index.tsx
+++ DUAN/src/pages/user/theme/header/index.tsx
@@ -1,19 +1,15 @@
 import { AiOutlineHeart, AiOutlineSearch, AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
 import "./style.css"
 import { Link } from "react-router-dom";
-import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <>
         <section className="container max-w-screen-xl m-auto flex item-center justify-between py-4">
             <img src="./logo.png" alt=""/>
             <ul className="flex gap-8 font-medium text-xl">
-                <li className="hover:hover:text-amber-500"><Link to=''>Shop</Link></li>
-                <li className="hover:hover:text-amber-500"><Link to='shop'>Home</Link></li>
-                <li className="hover:hover:text-amber-500"><a href="">About</a></li>
-                <li className="hover:hover:text-amber-500"><a href="">Contact</a></li>
-                <li className="hover:hover:text-amber-500"><Link to ="">Home</Link></li>
-                <li className="hover:hover:text-amber-500"><Link to ="">Shop</Link></li>
+                
+                <li className="hover:hover:text-amber-500"><Link to ="/">Home</Link></li>
+                <li className="hover:hover:text-amber-500"><Link to ="/shop">Shop</Link></li>
                 <li className="hover:hover:text-amber-500"><Link to ="">About</Link></li>
                 <li className="hover:hover:text-amber-500"><Link to ="">Contact</Link></li>
             </ul>
