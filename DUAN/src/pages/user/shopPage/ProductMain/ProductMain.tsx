0c74d0b6eb5daca3c6f51401334cdc0cd1ec7046
Thuan1611 <huythuan0977686@gmail.com>
2025-03-26 19:30:02 +0700

    loc san pham

--- DUAN/src/pages/user/shopPage/ProductMain/ProductMain.tsx
+++ DUAN/src/pages/user/shopPage/ProductMain/ProductMain.tsx
@@ -1,119 +1,29 @@
-import "./ProductMain.css"
-import { Link } from "react-router-dom"
+import "./ProductMain.css";
+import { Link } from "react-router-dom";
 
-const ProductMain = () => {
+const ProductMain = ({ filterProducts }) => {
   return (
+    
     <div className="grid grid-cols-3 gap-8">
-    <div>
-        <div className="overflow-hidden">
-            <Link to="/productdetail"><img src="/image 1.jpg" alt="" className="hover:scale-125 duration-1000"/></Link>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Syltherine</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Stylist cafe chair</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">2.500.000</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
+        {
+            filterProducts.map((item:any)=>{
+                return(
+                <div>
+                <div className="overflow-hidden">
+                    <Link to="/productdetail"><img src={item.image} alt="" className="hover:scale-125 duration-1000"/></Link>
+                </div>
+                <div className="bg-[#F5F5F5] p-4">
+                    <h3 className="font-semibold text-xl">{item.name}</h3>
+                    <p className="text-[#898989] text-base mt-1 mb-2">{item.description}</p>
+                    <p className="font-semibold text-xl text-red-600 mb-3">{item.price}</p>
+                    <button
+                        className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
+                </div>
+            </div>
+            )})
+        }
     </div>
-    <div>
-        <div className="overflow-hidden">
-            <img src="/image 2.jpg" alt="" className="hover:scale-125 duration-1000"/>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Leviosa</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Stylist cafe chair</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">1.800.000đ</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
-    </div>
-    <div>
-        <div className="overflow-hidden">
-            <img src="/image 3.jpg"  alt="" className="hover:scale-125 duration-1000"/>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Lolito</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Luxury big sofa</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">2.000.000</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
-    </div>
-    <div>
-        <div className="overflow-hidden">
-            <img src="/image 1.jpg"  alt="" className="hover:scale-125 duration-1000"/>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Syltherine</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Stylist cafe chair</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">2.500.000</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
-    </div>
-    <div>
-        <div className="overflow-hidden">
-            <img src="/image 2.jpg"  alt="" className="hover:scale-125 duration-1000"/>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Leviosa</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Stylist cafe chair</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">1.800.000đ</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
-    </div>
-    <div>
-        <div className="overflow-hidden">
-            <img src="/image 3.jpg"  alt="" className="hover:scale-125 duration-1000"/>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Lolito</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Luxury big sofa</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">2.000.000</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
-    </div>
-    <div>
-        <div className="overflow-hidden">
-            <img src="/image 1.jpg"  alt="" className="hover:scale-125 duration-1000"/>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Syltherine</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Stylist cafe chair</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">2.500.000</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
-    </div>
-    <div>
-        <div className="overflow-hidden">
-            <img src="/image 2.jpg"  alt="" className="hover:scale-125 duration-1000"/>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Leviosa</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Stylist cafe chair</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">1.800.000đ</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
-    </div>
-    <div>
-        <div className="overflow-hidden">
-            <img src="/image 3.jpg"  alt="" className="hover:scale-125 duration-1000"/>
-        </div>
-        <div className="bg-[#F5F5F5] p-4">
-            <h3 className="font-semibold text-xl">Lolito</h3>
-            <p className="text-[#898989] text-base mt-1 mb-2">Luxury big sofa</p>
-            <p className="font-semibold text-xl text-red-600 mb-3">2.000.000</p>
-            <button
-                className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:text-white hover:bg-yellow-700"><Link to = "/cart">Add to cart</Link></button>
-        </div>
-    </div>
-</div>
-  )
-}
+  );
+};
 
-export default ProductMain
+export default ProductMain;
